f76810dc5fded10c46f24612bda17f7fff325496
Schahriar SaffarShargh <info@schahriar.com>
2015-10-14 15:09:41 -0700

    Added Sample to test

--- test.js
+++ test.js
@@ -7,10 +7,37 @@ var expect = chai.expect;
 
 var client = new Spamc();
 
+var EASYSPAM = "Return-Path: ler@xample.example.com\n"
++ "Delivery-Date: Wed Sep 11 12:35:04 2002\n"
++ "Return-Path: <013516@aol.com>\n"
++ "Received: from lerami.lerctr.org (201.c98.etcenter.net [210.58.98.201])\n"
++ "	by example.example.com (8.12.2/8.12.2/20020902/$Revision: 1.30 $) with SMTP id g8BHYtE9023507;\n"
++ "	Wed, 11 Sep 2002 12:34:57 -0500 (CDT)\n"
++ "Message-Id: <200209111734.g8BHYtE9023507@example.example.com>\n"
++ "From: =?Big5?B?qfap9qXNrKG69A==?= <ee@example.com.tw>\n"
++ "Subject: =?Big5?B?rEKq96SjrE5+fqdPtsykRn5+?=\n"
++ "Content-Type: text/html\n"
++ "Date: Wed, 11 Sep 2002 17:19:10 +0800\n"
++ "X-Priority: 3\n"
++ "X-Library: Indy 9.0.3-B\n"
++ "To: undisclosed-recipients:;\n"
++ "\n"
++ "<HTML>\n"
++ "<BODY>\n"
++ '<iframe align="center" marginwidth=0 marginheight=0 src="http://www.callin.net/20020827" frameborder=0 width=800 scrolling=no height=1700 name="ticker"></iframe>\n'
++ "</BODY></HTML>";
+
 describe('Test Suite', function() {
-	it('should play ping-pong nicely', function(){
+	it('should play ping-pong nicely', function(done){
 		client.ping(function(error, didPlay) {
 			expect(didPlay).to.equal(true);
+			done();
+		})
+	})
+	it('should successfully run common commands', function() {
+		client.check(EASYSPAM, function(error, result) {
+			if(error) throw error;
+			expect(result.report).to.be.an('array');
 		})
 	})
 })
